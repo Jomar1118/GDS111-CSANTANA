b2feed58cf587400cd2afafbd232fbefcf45cc4d
Jomar1118 <jomarsantana1318@gmail.com>
2023-10-24 10:19:06 -0400

    Week 4b js

---
+++ Week4b.js
@@ -0,0 +1,16 @@
+let age =18;
+age = age + 1; // long-hand notation
+console.log("You are now"+ age  + " years old ");
+
+age++; //short-hand notation
+console.log("You are now" + age + "years old.");
+
+let ammo = 6;
+ammo = ammo -1;  
+ammo--;          
+console.log("Ammo left in the chamber: " + ammo);
+
+let balance = 0;
+balance = balance + 4.18; //price of milk
+balance = balance -0.50 // discount due to coupon
+console.log("You pay:" + balance);
