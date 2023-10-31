9023f0ef28d375c3a616fcaaa69cb6619897eb7d
Jomar1118 <jomarsantana1318@gmail.com>
2023-10-30 23:30:47 -0400

    Quest 4

---
+++ Ages.js
@@ -0,0 +1,15 @@
+console.log('World Of Warcraft');
+
+console.log('Minor 17 and below');
+console.log('Seniors are 65 and up');
+
+let age = 18;
+
+if (age >= 0 && age < 17) {
+    console.log('Minor');
+}
+else if (age >= 18 && age < 64) {
+    console.log('Neither Nenior nor Minor');
+}
+else
+console.log('Senior');
