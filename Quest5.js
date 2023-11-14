8187bbecbe847ac2cf5eb50c0935be4044f9e3b8
Jomar1118 <jomarsantana1318@gmail.com>
2023-11-13 22:00:02 -0500

    Quest 5

---
+++ Quest5.js
@@ -0,0 +1,26 @@
+console.log("Main Menu Bar");
+
+
+console.log("1.Change Actions");
+console.log("2.Button Layout");
+console.log("3.Keybindings");
+console.log("4.OnEvent Scripts");
+console.log("5.Main Menu Bar");
+console.log("6.Misc Options");
+console.log("7.Unlocking Dragging");
+console.log("8.Show Button IDs");
+console.log("Show Action IDs");
+console.log("10.Copy");
+console.log("11.Paste");
+
+function menu(){
+for (let p = 1; p < 12; p++){ 
+  if(p == 0) {break;}
+  
+} 
+let p=prompt("Your selection")
+
+}
+menu()
+
+  
